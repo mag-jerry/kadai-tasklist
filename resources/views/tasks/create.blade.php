2339c1ae2999d86bf9eb184e7878eb72be56ee0c
EC2 Default User <ec2-user@ip-172-31-3-175.us-east-2.compute.internal>
2018-07-26 01:27:51 +0000

    kadai-tasklist bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,26 @@
 
 <h1>作成ページ</h1>
 
- {!! Form::model($task,['route'=> 'tasks.store'])!!}
+ <div class="col-xs-12">
+  <div class="col-sm-offset-2 col-sm-8">
+   <div class="col-md-offset-2 col-md-8">
+    <div class="col-lg-offset-3 col-lg-6">
+       {!! Form::model($task, ['route' => 'tasks.store']) !!}
  
-  {!! Form::label('status', 'ステータス:') !!}
-  {!! Form::text('status') !!}
- </br>
-  {!! Form::label('content','メッセージ：') !!}
-  {!! Form::text('content') !!}
-  
-  {!! Form::submit('投稿') !!}
-  
- {!! Form::close() !!} 
+        <div class="form-group">
+         {!! Form::label('status', 'ステータス:') !!}
+         {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+         {!! Form::label('content', 'タスク:') !!}
+         {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+
+  {!! Form::submit('投稿', ['class' => 'btn btn-info']) !!}
+  {!! Form::close() !!}
+   </div>
+  </div>
+ </div>
+</div>
 
 @endsection
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,13 +5,26 @@
 <h1>タスク一覧</h1>
  
  @if(count($tasks) > 0)
-  <ul>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <td>ID</td>
+        <td>ステータス</td>
+        <td>タスク</td>
+      </tr>
+    </thead>
+    <tbody>
       @foreach($tasks as $task)
-      <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} :{{ $task->status }} > {{ $task->content }}</li>
+       <tr>
+         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+         <td>{{ $task->status }}</td>
+         <td>{{ $task->content }}</td>
+       </tr>
       @endforeach
-  </ul>
-  @endif
-  
-  {!! link_to_route('tasks.create', '新規メッセージの投稿') !!}
+    </tbody>
+  </table>
+ @endif
+ 
+ {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-success']) !!}
   
 @endsection
