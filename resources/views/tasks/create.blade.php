c5a1767d7f84e6c67f682425bc745a45cfd77947
EC2 Default User <ec2-user@ip-172-31-3-175.us-east-2.compute.internal>
2018-07-28 03:43:27 +0000

    kadai-tasklist Re:Bootstrap add_class=row

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,9 +4,11 @@
 
 <h1>作成ページ</h1>
 
- <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+ <div class="row">
+   <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+
        {!! Form::model($task, ['route' => 'tasks.store']) !!}
- 
+
         <div class="form-group">
          {!! Form::label('status', 'ステータス:') !!}
          {!! Form::text('status', null, ['class' => 'form-control']) !!}
@@ -15,9 +17,11 @@
          {!! Form::label('content', 'タスク:') !!}
          {!! Form::text('content', null, ['class' => 'form-control']) !!}
         </div>
-
+       
+       
   {!! Form::submit('投稿', ['class' => 'btn btn-info']) !!}
   {!! Form::close() !!}
-</div>
+ </div><!--col-->
+</div><!--row-->
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,6 +4,7 @@
 
 <h1>ID:{{ $task->id }}の編集ページ</h1>
 
+<div class="row">
  <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
   {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
      <div class="form-group">
@@ -18,7 +19,7 @@
 
        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
        {!! Form::close() !!}
-</div>
-
+ </div><!-- col -->
+</div><!-- row -->
 
 @endsection
