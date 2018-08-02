ff0fa31b72f2c5c637bbd86faaa42e8a30977576
EC2 Default User <ec2-user@ip-172-31-3-175.us-east-2.compute.internal>
2018-08-02 23:19:13 +0000

    tasklist user pages

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+Route::group(["middleware" => ["auth"]],function(){ 
+    Route::resource('users','Userscontroller',['only' => ['index','show']]);
+});
+
 
 Route::get('/','TasksController@index');
 Route::resource('tasks','TasksController');
